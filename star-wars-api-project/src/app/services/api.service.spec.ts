87c3b66beede05c9581cbf94eb4d721b72d77b26
Michele <boimik87@gmail.com>
2020-01-27 14:20:56 +0000

    refact

--- star-wars-api-project/src/app/services/api.service.spec.ts
+++ star-wars-api-project/src/app/services/api.service.spec.ts
@@ -41,48 +41,6 @@ describe('ApiService', () => {
 
 
   it('should get data', () => {
-    // const randomPeoplePage = Math.floor(Math.random() * 9) + 1;
-    // const randomStarshipsPage = Math.floor(Math.random() * 4) + 1;
-    //
-    //
-    // const dummyData = [
-    //   {
-    //     count: 87,
-    //     next: `https://swapi.co/api/people/?page=${randomPeoplePage + 1}` || null,
-    //     previous: `https://swapi.co/api/people/?page=${randomPeoplePage - 1}` || null,
-    //     results: new Array(87)
-    //   },
-    //   {
-    //     count: 37,
-    //     next: `https://swapi.co/api/starships/?page=${randomStarshipsPage + 1}` || null,
-    //     previous: `https://swapi.co/api/starships/?page=${randomStarshipsPage - 1}` || null,
-    //     results: new Array(37)
-    //   }
-    // ];
-    //
-    // service.getData().subscribe(data => {
-    //   expect(data.length).toBe(2);
-    //   expect(data).toEqual(dummyData);
-    //
-    //   const peopleReq = httpMock.expectOne(`https://swapi.co/api/people/?page=${randomPeoplePage}`);
-    //   const starshipReq = httpMock.expectOne(`https://swapi.co/api/starships/?page=${randomStarshipsPage}`);
-    //   forkJoin([peopleReq, starshipReq]).subscribe(e => {
-    //     expect(peopleReq.request.method).toBe('GET');
-    //     // expect(e[1].request.method).toBe('GET');
-    //     console.log(e);
-    //
-    //   });
-    //   peopleReq.flush(dummyData[0]);
-    //   starshipReq.flush(dummyData[1]);
-    //
-    //
-    //   expect(peopleReq.request.method).toBe('GET');
-    //   expect(starshipReq.request.method).toBe('GET');
-    //
-    //
-    //   peopleReq.flush(dummyData[0]);
-    //   starshipReq.flush(dummyData[1]);
-    // });
 
     const randomPeoplePage = Math.floor(Math.random() * 9) + 1;
     const randomStarshipsPage = Math.floor(Math.random() * 4) + 1;
@@ -95,21 +53,18 @@ describe('ApiService', () => {
       starshipReq: {data: starshipReq}
     };
 
-    // Assuming this is imported..
     class ApiServ {
-      constructor(private http) {
+      constructor(private http: HttpClientModule) {
       }
 
       getData() {
       }
     }
 
-    service.getData();
-
     const getSpy = jasmine.createSpy('Http.get').and.callFake((arg) => {
       return of(responseMap[arg]);
     });
-    const mockHttp = {get: getSpy}
+    const mockHttp = {get: getSpy};
     const apiService = new ApiServ(mockHttp);
 
     spyOn(apiService, 'getData').and.callFake(() =>
